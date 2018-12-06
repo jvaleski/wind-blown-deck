295d4a175c3d06fe505f0f1508fe06a6edfff9d3
Jud Valeski <jvaleski@gmail.com>
2018-12-06 15:16:27 -0700

    adding configuration file support and reading image data from a URL

--- predict.py
+++ predict.py
@@ -1,4 +1,6 @@
 import sys
+import urllib2
+import json
 
 from google.cloud import automl_v1beta1
 from google.cloud.automl_v1beta1.proto import service_pb2
@@ -15,29 +17,19 @@ def get_prediction(content, project_id, model_id):
   return request  # waits till request is returned
 
 if __name__ == '__main__':
-  file_path = sys.argv[1]
-  project_id = sys.argv[2]
-  model_id = sys.argv[3]
-  twilio_acct_sid = sys.argv[4]
-  twilio_auth_token = sys.argv[5]
-  destination_num = sys.argv[6]
-  from_num = sys.argv[7]
+  with open('config.json') as json_data_file:
+    cfg = json.load(json_data_file)
 
-  with open(file_path, 'rb') as ff:
-    content = ff.read()
+  content = urllib2.urlopen(cfg['other']['image-url']).read()
 
-  prediction = get_prediction(content, project_id,  model_id)
-  
-  print(prediction)
+  prediction = get_prediction(content, cfg['automl']['project-id'],  cfg['automl']['model-id'])
 
-  twilio_client = Client(twilio_acct_sid, twilio_auth_token)
+  twilio_client = Client(cfg['twilio']['account-sid'], cfg['twilio']['auth-token'])
 
   message = twilio_client.messages.create(
-                              from_=from_num,
+                              from_=cfg['twilio']['from-num'],
                               body=prediction,
-                              to=destination_num
+                              to=cfg['twilio']['to-num']
                           )
-
-  print(message.sid)
 
   
